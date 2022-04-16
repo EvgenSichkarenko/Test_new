6821531dba1a153d742b5847ccadeab672c126a1
QAEvgen <evgens@superiorcsgroup.com>
2022-04-16 11:13:25 +0300

    add options

--- pages/application.py
+++ pages/application.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from pages.login import Login
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 
 class Application:
 	def	__init__(self):
 		#self.wd = webdriver.Chrome(executable_path="C:\\Python_project\\Test_Sotka\\drivers\\chromedriver.exe")
-		self.wd = webdriver.Chrome(executable_path="/home/ubuntu/drivers/chromedriver")
+		#self.wd = webdriver.Chrome(executable_path="/home/ubuntu/drivers/chromedriver")
+		self.wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 		self.login = Login(self)
 
 
