b17e66d23d30be43fa94213352e4f4b55e491cfb
QAEvgen <evgens@superiorcsgroup.com>
2022-04-16 08:48:00 +0300

    add paht

--- pages/application.py
+++ pages/application.py
@@ -5,7 +5,7 @@ from pages.login import Login
 class Application:
 	def	__init__(self):
 		#self.wd = webdriver.Chrome(executable_path="C:\\Python_project\\Test_Sotka\\drivers\\chromedriver.exe")
-		self.wd = webdriver.Chrome(executable_path="/drivers/chromedriver")
+		self.wd = webdriver.Chrome(executable_path="/home/ubuntu/drivers/chromedriver")
 		self.login = Login(self)
 
 
